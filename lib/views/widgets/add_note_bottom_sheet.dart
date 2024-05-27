b2f8120b03a8f96d2f9226125c29116cba4b30be
moelsayed1 <mostafaembaby19@gmail.com>
2024-05-27 20:58:38 +0300

    finish add note bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -7,13 +8,23 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 32),
-      child: Column(
-        children: [
-          CustomTextField(),
-        ],
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(height: 15),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(height: 15),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
 }
-
---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+        ),
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
